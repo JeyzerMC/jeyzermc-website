0fc16f93bc0e24d66a2f5783e191ef2b9c78b723
Jeyzer <jeyzermc@github.com>
2018-07-08 17:00:20 -0400

    TS Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Mehdi Chaid',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-sass', 
+    'gatsby-plugin-typescript'
+  ],
 }
