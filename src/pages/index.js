fb98c8071df4708045a41501669a1f5bf63ad8c6
Jeyzer <jeyzermc@github.com>
2018-07-08 18:29:51 -0400

    Initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import MehdiImg from '../../res/images/me.jpg'
+import styles from "./index.module.css";
+
 const IndexPage = () => (
   <div>
-    <h1>I'm Jeyzer</h1>
+    <h1 className={styles.user}>Hello, I'm Mehdi Chaid</h1>
     <p>Welcome to my website! The work is in progress.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+
+    <img  src={MehdiImg}/>
   </div>
 )
 
---
+++ src/pages/testwb.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>This is a test wb</h1>
+    
+    <Link to="/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
