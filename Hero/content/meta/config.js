d36ee086d771f998c626350475cfe11968030f50
Jeyzer <jeyzermc@github.com>
2018-08-17 17:57:24 -0400

    New Theme

---
+++ Hero/content/meta/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteTitle: "Mehdi Chaid, software engineering student", // <title>
+  shortSiteTitle: "Mehdi Chaid, software engineering stuent", // <title> ending for posts and pages
+  siteDescription: "Personal blog of Mehdi Chaid",
+  siteUrl: "https://jeyzermc.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "jeyzermc",
+  authorTwitterAccount: "jeyzermc",
+  // info
+  headerTitle: "Mehdi Chaid",
+  headerSubTitle: "software engineering student",
+  // manifest.json
+  manifestName: "Mehdi Chaid, softare engineering student",
+  manifestShortName: "Mehdi Chaid", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "black",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/JeyzerMC" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" },
+    { name: "linkedin", url: "http://linkedin.com/in/mehdi-chaid" },
+    { name: "devpost", url: "http://devpost.com/Jeyzer" }
+  ]
+};
--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Mehdi Chaid, software developer',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
