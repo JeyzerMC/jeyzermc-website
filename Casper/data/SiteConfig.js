583751a02259268a4c570874093abddd69e1e934
Jeyzer <jeyzermc@github.com>
2018-08-17 22:57:50 -0400

    full screen for mobile

--- Casper/data/SiteConfig.js
+++ Casper/data/SiteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   siteUrl: "https://jeyzermc.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription:
-    "My personnal blog for software development projects", // Website description used for RSS feeds/meta description tag.
+    "My personnal blog for software development projects [ON PROGRESS]", // Website description used for RSS feeds/meta description tag.
   siteCover:
     "/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
   siteNavigation: true, // If navigation is enabled the Menu button will be visible
@@ -20,36 +20,36 @@ module.exports = {
   googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
   // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
   siteSocialUrls: [
-    "https://github.com/haysclark/gatsby-starter-casper",
-    "https://twitter.com/gatsbyjs",
-    "mailto:gatsbyjs@example.com"
+    "https://github.com/JeyzerMC",
+    "https://linkedin.com/in/mehdi-chaid",
+    "mailto:jeyzermc@gmail.com"
   ],
   postDefaultCategoryID: "Tech", // Default category for posts.
   // Links to social profiles/projects you want to display in the navigation bar.
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/haysclark/gatsby-starter-casper",
+      url: "https://github.com/JeyzerMC",
       iconClassName: "fa fa-github" // Disabled, see Navigation.jsx
     },
     {
-      label: "Twitter",
-      url: "https://twitter.com/gatsbyjs",
-      iconClassName: "fa fa-twitter" // Disabled, see Navigation.jsx
+      label: "LinkedIn",
+      url: "https://linkedin.com/in/mehdi-chaid",
+      iconClassName: "fa fa-linkedin" // Disabled, see Navigation.jsx
     },
     {
       label: "Email",
-      url: "mailto:gatsbyjs@example.com",
+      url: "mailto:jeyzermc@gmail.com",
       iconClassName: "fa fa-envelope" // Disabled, see Navigation.jsx
     }
   ],
   // Copyright string for the footer of the website and RSS feed.
   copyright: {
-    label: "Gatsby Casper Starter" // Label used before the year
+    label: "Mehdi Chaid" // Label used before the year
     // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
     // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
   },
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0", // Used for setting manifest background color.
-  promoteGatsby: true // Enables the GatsbyJS promotion information in footer.
+  promoteGatsby: false // Enables the GatsbyJS promotion information in footer.
 };
