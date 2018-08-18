f7b58d06aeb799fce5b965b99a01d7ddf85466aa
Jeyzer <jeyzermc@github.com>
2018-08-17 23:22:42 -0400

    Change to Casper

---
+++ data/SiteConfig.js
@@ -0,0 +1,55 @@
+module.exports = {
+  blogPostDir: "sample-posts", // The name of directory that contains your posts.
+  blogAuthorDir: "sample-authors", // The name of directory that contains your 'authors' folder.
+  blogAuthorId: "casper", // The default and fallback author ID used for blog posts without a defined author.
+  siteTitle: "Mehdi Chaid, software engineering student", // Site title.
+  siteTitleAlt: "Mehdi Chaid, software engineering student", // Alternative site title for SEO.
+  siteLogo:
+    "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
+  siteUrl: "https://jeyzermc.com", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
+  siteDescription:
+    "My personnal blog for software development projects [ON PROGRESS]", // Website description used for RSS feeds/meta description tag.
+  siteCover:
+    "/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
+  siteNavigation: true, // If navigation is enabled the Menu button will be visible
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteRssAuthor: "Casper User", // The author name used in the RSS file
+  // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
+  sitePaginationLimit: 10, // The max number of posts per page.
+  googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
+  // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
+  siteSocialUrls: [
+    "https://github.com/JeyzerMC",
+    "https://linkedin.com/in/mehdi-chaid",
+    "mailto:jeyzermc@gmail.com"
+  ],
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  // Links to social profiles/projects you want to display in the navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/JeyzerMC",
+      iconClassName: "fa fa-github" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "LinkedIn",
+      url: "https://linkedin.com/in/mehdi-chaid",
+      iconClassName: "fa fa-linkedin" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "Email",
+      url: "mailto:jeyzermc@gmail.com",
+      iconClassName: "fa fa-envelope" // Disabled, see Navigation.jsx
+    }
+  ],
+  // Copyright string for the footer of the website and RSS feed.
+  copyright: {
+    label: "Mehdi Chaid" // Label used before the year
+    // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
+    // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
+  },
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  promoteGatsby: false // Enables the GatsbyJS promotion information in footer.
+};
