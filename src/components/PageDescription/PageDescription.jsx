4c45f1477a445a46ac020bb5f5a83fee57e01433
Jeyzer <jeyzermc@github.com>
2018-08-31 13:30:19 -0400

    Changed description to typist

--- src/components/PageDescription/PageDescription.jsx
+++ src/components/PageDescription/PageDescription.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./PageDescription.css";
+import TypistCycle from "../TypistCycle/TypistCycle"
 
 class PageDescription extends React.Component {
   render() {
     const { text } = this.props;
     if (text) {
-      return <h2 className="page-description">{text}</h2>;
+      return (
+        // <h2 className="page-description">
+        //   {text}
+        // </h2>
+        <TypistCycle
+          content={[
+            'Engineering student',
+            'Machine Learning enthusiast',
+            'Game developer',
+          ]}
+          numberOfCycles={-1} // loop indefinitely
+          segmentDelay={0.8} // stop for 0.8s at end line
+          className="typist-description"
+        />
+      );
     }
     return null;
   }
